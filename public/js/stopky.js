b596a307a286990ea5e8968b21ea3581507d592d
Gorazd <gstibi@gmail.com>
2021-02-27 23:59:50 +0100

    dorobene stopky a rozlozenie

---
+++ public/js/stopky.js
@@ -0,0 +1,41 @@
+/* var currentTime = 90
+var interval;
+var started = false */
+
+var timer = document.getElementById("timer")
+var start = document.getElementById("start")
+var reset = document.getElementById("reset")
+
+timer.currentTime = 90
+timer.interval;
+timer.started = false
+
+start.addEventListener("click", () => {
+    if(!timer.started){
+        timer.interval = setInterval(decreaseTime, 1000, timer)
+        timer.started = true
+    }
+})
+
+reset.addEventListener("click", () => {
+    resetTimer(timer)
+})
+
+function decreaseTime(element){
+    element.currentTime -= 1
+    element.innerHTML = element.currentTime
+    if(element.currentTime <= 0){
+        stopTimer(element)
+    }
+}
+
+function resetTimer(element){
+    stopTimer(element)
+    element.innerHTML = element.currentTime
+}
+
+function stopTimer(element){
+    clearInterval(element.interval)
+    element.started = false
+    element.currentTime = 90
+}
